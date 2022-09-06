d5baa7a5ab961ed6f3cf37716039425d4d04d7e7
kaen7 <kusdsuna@naver.com>
2022-09-06 16:27:46 +0900

    chore: information logic

--- src/lib/thegraph/service.ts
+++ src/lib/thegraph/service.ts
@@ -1,19 +1,18 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { SUPPORTED_NETWORKS } from 'configure/network'
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { SUPPORTED_NETWORKS } from "configure/network";
 
-export const get = ({url, query, mapping, variables, errorCallback, networkId}) => {
-    return new ApolloClient({
-        uri: process.env[`REACT_APP_${SUPPORTED_NETWORKS[networkId]}_THEGRAPH_URL`] + url,
-        cache: new InMemoryCache(),
-        connectToDevTools: true
-    }).query({
-        query,
-        variables
-    })
-    .then((data) => mapping(data))
-    .catch(errorCallback)
-}
-
-
-
-
+export const get = ({ url, query, mapping, variables, errorCallback, networkId }) => {
+	// uri: process.env[`REACT_APP_${SUPPORTED_NETWORKS[networkId]}_THEGRAPH_URL`] + url,
+	console.log(SUPPORTED_NETWORKS[networkId]);
+	return new ApolloClient({
+		uri: process.env[`REACT_APP_THEGRAPH_URL`] + url,
+		cache: new InMemoryCache(),
+		connectToDevTools: true,
+	})
+		.query({
+			query,
+			variables,
+		})
+		.then((data) => mapping(data))
+		.catch(errorCallback);
+};
--- src/pages/Stake/Stake.tsx
+++ src/pages/Stake/Stake.tsx
@@ -11,18 +11,14 @@ import { setPeriholderCounts } from "reducers/periholderCounts";
 import { setExchangeRates } from "reducers/exchangeRates";
 import { setPeriRates } from "reducers/periRates";
 import {
-  getStableDebt,
-  getChartRates,
-  getDebtCaches,
-  getPeriholderCounts,
-  getLastRates,
-  getLastPeriRates
+	getStableDebt,
+	getChartRates,
+	getDebtCaches,
+	getPeriholderCounts,
+	getLastRates,
+	getLastPeriRates,
 } from "lib/thegraph/api";
-import {
-  getCachedDebts,
-  getTotalAPY,
-  getTotalCirculatingSupply,
-} from "lib/api/api";
+import { getCachedDebts, getTotalAPY, getTotalCirculatingSupply } from "lib/api/api";
 
 import StakingByAssets from "screens/Stake/StakingByAssets";
 import DebtByNetworks from "screens/Stake/DebtByNetworks";
@@ -31,98 +27,98 @@ import Information from "screens/Stake/Information";
 import Holders from "screens/Stake/Holders";
 
 const Stake = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { stakeIsReady } = useSelector((state: RootState) => state.app);
+	const { stakeIsReady } = useSelector((state: RootState) => state.app);
 
-  const getPERIandStalbeDebt = (value) => {
-    let stable = 0n;
-    Object.keys(value).forEach((e) => {
-      if (e !== "total") {
-        stable = stable + value[e];
-      }
-    });
-    return {
-      stable,
-      PERI: value["total"] - stable,
-      ...value,
-    };
-  };
+	const getPERIandStalbeDebt = (value) => {
+		let stable = 0n;
+		Object.keys(value).forEach((e) => {
+			if (e !== "total") {
+				stable = stable + value[e];
+			}
+		});
+		return {
+			stable,
+			PERI: value["total"] - stable,
+			...value,
+		};
+	};
 
-  const getDebts = () => {
-    const promise = [getCachedDebts(), getStableDebt()];
-    const debts = Promise.all(promise).then((data) => {
-      const value = {};
-      Object.keys(data[0]).forEach((e) => {
-        value[e] = getPERIandStalbeDebt(Object.assign(data[0][e], data[1][e]));
-      });
-      return value;
-    });
-    return debts;
-  };
-  const init = async () => {
-    const [
-      debt,
-      apy,
-      chartRate,
-      circulatingSupply,
-      networkByDebtCashes,
-      periholderCounts,
-      exchangeRates,
-      periRates,
-    ] = await Promise.all([
-      getDebts(),
-      getTotalAPY(),
-      getChartRates({
-        currencyName: "PERI",
-        networkId: process.env.REACT_APP_ENV === "production" ? 137 : 80001,
-      }),
-      getTotalCirculatingSupply(),
-      getDebtCaches(),
-      getPeriholderCounts(),
-      getLastRates(),
-      getLastPeriRates(),
-    ]);
+	const getDebts = () => {
+		const promise = [getCachedDebts(), getStableDebt()];
+		const debts = Promise.all(promise).then((data) => {
+			const value = {};
+			Object.keys(data[0]).forEach((e) => {
+				value[e] = getPERIandStalbeDebt(Object.assign(data[0][e], data[1][e]));
+			});
+			return value;
+		});
+		return debts;
+	};
+	const init = async () => {
+		const [
+			debt,
+			apy,
+			chartRate,
+			circulatingSupply,
+			networkByDebtCashes,
+			periholderCounts,
+			exchangeRates,
+			periRates,
+		] = await Promise.all([
+			getDebts(),
+			getTotalAPY(),
+			getChartRates({
+				currencyName: "PERI",
+				networkId: process.env.REACT_APP_ENV === "production" ? 137 : 80001,
+			}),
+			getTotalCirculatingSupply(),
+			getDebtCaches(),
+			getPeriholderCounts(),
+			getLastRates(),
+			getLastPeriRates(),
+		]);
 
-    dispatch(setExchangeRates(exchangeRates));
-    dispatch(setPeriRates(periRates));
-    dispatch(setNetworkCachedDebts(debt));
-    dispatch(setAPY(apy));
-    dispatch(setPeriChartRates(chartRate));
-    dispatch(setCirculatingSupply(circulatingSupply));
-    dispatch(setNetworkByDebtCashes(networkByDebtCashes));
-    dispatch(setPeriholderCounts(periholderCounts));
-    dispatch(setStakeIsReady());
-  };
+		dispatch(setExchangeRates(exchangeRates));
+		dispatch(setPeriRates(periRates));
+		dispatch(setNetworkCachedDebts(debt));
+		dispatch(setAPY(apy));
+		dispatch(setPeriChartRates(chartRate));
+		dispatch(setCirculatingSupply(circulatingSupply));
+		dispatch(setNetworkByDebtCashes(networkByDebtCashes));
+		dispatch(setPeriholderCounts(periholderCounts));
+		dispatch(setStakeIsReady());
+	};
 
-  useEffect(() => {
-    if (stakeIsReady === false) {
-      init();
-    }
-  }, [stakeIsReady]);
+	useEffect(() => {
+		if (stakeIsReady === false) {
+			init();
+		}
+	}, [stakeIsReady]);
 
-  return (
-    <div className="flex flex-col px-4 lg:px-0 gap-5">
-      <div className="flex flex-col lg:flex-row gap-5">
-        <div className="lg:h-86">
-          <StakingByAssets></StakingByAssets>
-        </div>
-        <div className="lg:h-86 lg:flex-1">
-          <DebtByNetworks></DebtByNetworks>
-        </div>
-      </div>
-      <div className="flex flex-col lg:flex-row gap-5">
-        <div className="lg:h-60 flex-1">
-          <Overview></Overview>
-        </div>
-        <div className="lg:h-60">
-          <Information></Information>
-        </div>
-        <div className="lg:h-60">
-          <Holders></Holders>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col px-4 lg:px-0 gap-5">
+			<div className="flex flex-col lg:flex-row gap-5">
+				<div className="lg:h-86">
+					<StakingByAssets></StakingByAssets>
+				</div>
+				<div className="lg:h-86 lg:flex-1">
+					<DebtByNetworks></DebtByNetworks>
+				</div>
+			</div>
+			<div className="flex flex-col lg:flex-row gap-5">
+				<div className="lg:h-60 flex-1">
+					<Overview></Overview>
+				</div>
+				<div className="lg:h-60">
+					<Information></Information>
+				</div>
+				<div className="lg:h-60">
+					<Holders></Holders>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Stake;
