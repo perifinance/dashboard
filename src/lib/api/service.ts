04b6862d91177ecc36b1df69bb427fbaff37bedf
kaen7 <kusdsuna@naver.com>
2022-09-21 17:25:43 +0900

    chore: update live DB endpoint

--- src/lib/api/service.ts
+++ src/lib/api/service.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const get = ({ url, params, mapping }) => {
-	// const API_URL = "https://dex-api.peri.finance/api/v1/";
-	const API_URL = "http://localhost:4001/api/v1/";
+	const API_URL = "https://dex-api.peri.finance/api/v1/";
+	// const API_URL = "http://localhost:4001/api/v1/";
 
 	axios.defaults.withCredentials = true;
 	return axios({
--- src/lib/thegraph/service.ts
+++ src/lib/thegraph/service.ts
@@ -3,8 +3,8 @@ import { SUPPORTED_NETWORKS } from "configure/network";
 
 export const get = ({ url, query, mapping, variables, errorCallback, networkId }) => {
 	return new ApolloClient({
-		// uri: "https://dex-api.peri.finance/dex/", // ! 임시 주석처리
-		uri: "http://localhost:4000/",
+		uri: "https://dex-api.peri.finance/", // ! 임시 주석처리
+		// uri: "http://localhost:4000/",
 		cache: new InMemoryCache(),
 		connectToDevTools: true,
 	})
